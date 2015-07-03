908b10c489792db2af47ec12dd00b0d269eacd0f
Arem_Prokhatskyi <Arem_Prokhatskyi@epam.com>
2015-07-03 12:04:29 +0300

    deleted commented text

--- js/app.js
+++ js/app.js
@@ -1,22 +1,3 @@
-//window.game = new window.VGN.Game();
-//
-//setInterval(function() {
-//    var canvas = game.getCanvasSize();
-//    var player = game.player.getPosition();
-//    var food = game.food;
-//
-//    var html = '<h1>The Game</h1>' +
-//        '<p> Canvas size: ' + canvas.width + ' x ' + canvas.height + ' </p>' +
-//        '<h2>Player</h2>' +
-//        '<p> Position: X = ' + player.x + '; Y = ' + player.y + ' </p>';
-//
-//    for(var i = 0; i < food.length; i++) {
-//        var type =  food[i].getType();
-//        var pos = food[i].getPosition();
-//        html += '<h3>Meal</h3><p>Type:' + type + '<br>Position: X = ' + pos.x + '; Y = ' + pos.y + '</p>'
-//    }
-//    document.body.innerHTML = html;
-//}, 10);
 'use strict';
 
 (function() {
